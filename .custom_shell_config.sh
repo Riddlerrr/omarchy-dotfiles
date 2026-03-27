1b9c5e54eba1ce93de10d2bfb67da5b327bc5c31
Sergei Andronov <sergey@andronov.me>
2026-03-27 09:12:23 +0100

    zsh: remove global main dev function

--- .custom_shell_config.sh
+++ .custom_shell_config.sh
@@ -13,9 +13,6 @@ fi
 
 alias os='echo $OS'
 
-# Command's shortcut
-alias h='helix'
-
 # Rails
 alias be='bundle exec'
 alias rdm="bin/rails db:migrate"
@@ -72,29 +69,6 @@ if [[ -n "$ZSH_VERSION" ]]; then
 fi
 
 load_local_aliases
-
-dev() {
-  case "$1" in
-    start)
-		  if [[ -z "$DEV_START" ]]; then
-				echo "Error: ENV DEV_START is not defined"
-				return 1
-      fi
-      eval "$DEV_START"
-      ;;
-    stop)
-		  if [[ -z "$DEV_STOP" ]]; then
-				echo "Error: ENV DEV_STOP is not defined"
-				return 1
-      fi
-      eval "$DEV_STOP"
-      ;;
-    *)
-      echo "Usage: dev {start|stop}"
-      return 1
-      ;;
-  esac
-}
 
 lg() {
   if [ "$1" = "recent" ]; then
